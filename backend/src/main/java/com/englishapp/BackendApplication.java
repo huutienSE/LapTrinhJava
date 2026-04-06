b4b0e33759144e17f66a5dc6b4f4b492730c4e4c
huutienSE <huutiense25@gmail.com>
2026-04-06 21:02:28 +0700

    refactor project structure and organize packages

--- backend/src/main/java/com/englishapp/BackendApplication.java
+++ backend/src/main/java/com/englishapp/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.englishapp.backend;
+package com.englishapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/main/java/com/englishapp/controller/controller/TestController.java
+++ backend/src/main/java/com/englishapp/controller/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.englishapp.backend.controller;
+package com.englishapp.controller.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
