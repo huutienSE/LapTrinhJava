75b72f5175cdad237eee5cbcb62a9f7dcbdd2b4a
huutienSE <huutiense25@gmail.com>
2026-04-29 00:21:18 +0700

    chore: add getCurrentUser method into AuthController

--- backend/src/main/java/com/englishapp/controller/AuthController.java
+++ backend/src/main/java/com/englishapp/controller/AuthController.java
@@ -1,16 +1,16 @@
 package com.englishapp.controller;
 
 import com.englishapp.common.ApiResponse;
-import com.englishapp.dto.auth.LoginRequest;
-import com.englishapp.dto.auth.LoginResponse;
-import com.englishapp.dto.auth.RegisterRequest;
-import com.englishapp.dto.auth.RegisterResponse;
+import com.englishapp.dto.auth.*;
+import com.englishapp.security.UserPrincipal;
 import com.englishapp.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -37,13 +37,22 @@ public class AuthController {
     }
 
     @GetMapping("/me")
-    public ApiResponse<Object> getCurrentUser() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    public ApiResponse<CurrentUserResponse> getCurrentUser(Authentication authentication) {
 
-        return new ApiResponse<>(
-                true,
-                auth,
-                "Current user info"
+        UserPrincipal user = (UserPrincipal) authentication.getPrincipal();
+
+        CurrentUserResponse response = CurrentUserResponse.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .roles(
+                        user.getAuthorities()
+                                .stream()
+                                .map(a -> a.getAuthority())
+                                .collect(Collectors.toList())
+                )
+                .build();
+
+        return new ApiResponse<>(true, response, "Current user info"
         );
     }
 }
