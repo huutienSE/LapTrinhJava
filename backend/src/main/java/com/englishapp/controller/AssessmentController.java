2a4931f089f723ded4a52db9119711cff2855c93
nhanhehehe <bengame2006@gmail.com>
2026-05-08 16:31:33 +0700

    bullshit cua Tien

--- backend/src/main/java/com/englishapp/controller/AssessmentController.java
+++ backend/src/main/java/com/englishapp/controller/AssessmentController.java
@@ -7,31 +7,31 @@ import com.englishapp.service.AssessmentService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-//@RestController
-//@AllArgsConstructor
-//@CrossOrigin(origins = "http://localhost:5173")
-//@RequestMapping("/api/assessment")
-//public class AssessmentController {
-//
-//    private final AssessmentService assessmentService;
-//
-//    @GetMapping("/start")
-//    public ApiResponse<Object> startAssessment(@RequestBody StartAssessmentRequest request) {
-//        Integer sessionId = assessmentService.startAssessment(request);
-//
-//        return new ApiResponse<>(
-//                true,
-//                sessionId,
-//                "Start assessment successfully"
-//        );
-//    }
-//
-//    @PostMapping("/commit")
-//    public ApiResponse<Object> commitAssessment(@RequestBody CommitAssessmentRequest request) {
-//        return new ApiResponse<>(
-//                true,
-//                assessmentService.commitAssessment(request),
-//                "Commit assessment successfully"
-//        );
-//    }
-//}
+@RestController
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/api/assessment")
+public class AssessmentController {
+
+    private final AssessmentService assessmentService;
+
+    @GetMapping("/start")
+    public ApiResponse<Object> startAssessment(@RequestBody StartAssessmentRequest request) {
+        Integer sessionId = assessmentService.startAssessment(request);
+
+        return new ApiResponse<>(
+                true,
+                sessionId,
+                "Start assessment successfully"
+        );
+    }
+
+    @PostMapping("/commit")
+    public ApiResponse<Object> commitAssessment(@RequestBody CommitAssessmentRequest request) {
+        return new ApiResponse<>(
+                true,
+                assessmentService.commitAssessment(request),
+                "Commit assessment successfully"
+        );
+    }
+}
