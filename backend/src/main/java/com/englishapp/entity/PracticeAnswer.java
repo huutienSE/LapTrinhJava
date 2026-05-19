9f3fca9e227a83294195aa88f5976e3a708cb500
nhanhehehe <bengame2006@gmail.com>
2026-05-20 03:55:06 +0700

    tranction data var -> test

--- backend/src/main/java/com/englishapp/entity/PracticeAnswer.java
+++ backend/src/main/java/com/englishapp/entity/PracticeAnswer.java
@@ -24,7 +24,7 @@ public class PracticeAnswer {
     })
     private PracticeQuestion practiceQuestion;
 
-    @Column(name = "user_answer")
+    @Column(name = "user_answer", columnDefinition = "TEXT")
     private String userAnswer;
 
     @Column(name = "created_date")
--- database/AESP_DATABASE.sql
+++ database/AESP_DATABASE.sql
@@ -196,7 +196,7 @@ CREATE TABLE practice_answer
     answer_id    INT PRIMARY KEY AUTO_INCREMENT,
     session_id   INT NOT NULL,
     question_id  INT NOT NULL,
-    user_answer  VARCHAR(100),
+    user_answer  TEXT,
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP(),
 
     FOREIGN KEY (session_id, question_id)
@@ -212,7 +212,7 @@ CREATE TABLE IF NOT EXISTS feedback
     feedback_id   INT PRIMARY KEY AUTO_INCREMENT,
     answer_id     INT UNIQUE,
     overall_score INT CHECK (overall_score BETWEEN 0 AND 100),
-    feedback_text VARCHAR(200),
+    feedback_text TEXT,
     created_date  DATETIME DEFAULT (CURRENT_TIMESTAMP()),
     FOREIGN KEY (answer_id)
         REFERENCES practice_answer (answer_id)
